297e12792bdbd783b2d86442944a4e8d13fc40ec
Deva Kumar Vishwakarma <112493770+DevaKumar143@users.noreply.github.com>
2022-09-08 17:32:06 +0530

    average of three number

---
+++ main.c
@@ -0,0 +1,13 @@
+
+// write a program to print the avg of 3 number 
+
+#include<stdio.h>
+ 
+int main(){
+    int x,y,z;
+    printf("Enter a number");
+    scanf("%d %d %d",&x,&y,&z);
+
+    printf("The avg of three number is %d",(x+y+z)/3);
+    return 0;
+}
